b80277f39a14191ef6fdc11ba413e35fb8b69b13
Nadien-Rashad <42560282+Nadien-Rashad@users.noreply.github.com>
2021-06-12 03:34:35 +0200

    Create Add_pit.py

---
+++ Functions/Add_pit.py
@@ -0,0 +1,8 @@
+def Add_pit(value):
+    """
+    add the value by one when it’s called 
+    Returns the value to be stored in the pocket or the store
+    """
+    temp_value = value
+    bin = temp_value+1
+    return bin
